a657513925ceaac331ea92e1610f7f40ea447773
Reavershark <jonasty1999@gmail.com>
2020-02-13 10:01:18 +0100

    data & size query parameters

--- source/app.d
+++ source/app.d
@@ -1,10 +1,19 @@
 import vibe.vibe;
 import vibe.http.server;
+import std.range;
+
+immutable ubyte[] default_data = [cast(ubyte) '0'];
+immutable ulong default_size = 1_000_000;
 
 void handleRequest(scope HTTPServerRequest req, scope HTTPServerResponse res)
 {
 	enforce(req.method == HTTPMethod.GET);
-	res.bodyWriter.write("Hello world!");
+
+	immutable ubyte[] data = ("data" in req.query) ? cast(immutable ubyte[]) req
+		.query["data"] : default_data;
+	immutable ulong size = ("size" in req.query) ? req.query["size"].to!ulong : default_size;
+
+	res.writeBody(data.repeat.take(size).join.array);
 }
 
 shared static this()
@@ -12,7 +21,6 @@ shared static this()
 	auto settings = new HTTPServerSettings;
 	settings.port = 8080;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
-	settings.sessionStore = new MemorySessionStore;
 
 	listenHTTP(settings, &handleRequest);
 }
